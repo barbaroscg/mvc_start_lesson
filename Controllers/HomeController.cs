c5823b107a9fb1f55e020791177a143a9b6a51b7
barbaroscg <barb.ciga@gmail.com>
2024-01-03 21:54:00 +0300

    sections

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Contact()
     {
         return View();
     }
